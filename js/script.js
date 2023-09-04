f98c3ac872323148d1e31afc767439e5838cddb9
Jharul-Islam <sahin22@gmail.com>
2023-09-04 10:05:31 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+let searchInput = document.querySelector('#searchUser')
+let searchBtn = document.querySelector('#searchBtn');
+let ui = new UI();
+
+searchBtn.addEventListener('click', (e) =>{
+    let userText = searchInput.value;
+    if(userText != ''){
+        fetch(`https://api.github.com/users/${userText}`)
+            .then(res => res.json())
+            .then(data =>{
+                console.log(data);
+                if(data.message == 'Not Found'){
+                    //show alert
+                    ui.showAlert('User Not found !!', 'alert alert-danger');
+                }else{
+                    //show profile
+                    ui.showProfile(data);
+                }
+            })
+
+    }else{
+        ui.clearProfile();
+    }
+})
